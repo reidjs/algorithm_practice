7f0e3bb1bdda2215cbb2fbcddadc48aa0315ced6
reid <reidsherman@gmail.com>
2017-12-24 12:17:03 -0800

    Dynamic array passes spec

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -4,31 +4,50 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @length = 0
+    @store = []
+    @capacity = 8
   end
 
   # O(1)
   def [](index)
+    raise "index out of bounds" if index >= @length
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if @store.empty?
+    @length -= 1
+    @store.pop
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    @length += 1
+    resize! if @length > capacity
+    @store << val
+    # @capacity += 1
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if @store.empty?
+
+    @length -= 1
+    @store.shift 
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    @length += 1
+    @store.unshift(val)
   end
 
   protected
@@ -40,5 +59,10 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    # @temp_store = []
+    # @store.each do |el|
+      
+    # end 
+    @capacity = @capacity * 2 
   end
 end
