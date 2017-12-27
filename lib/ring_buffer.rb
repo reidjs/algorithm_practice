a3fd00b9b1361f7453ccc8320fd080e50840d3a8
reid <reidsherman@gmail.com>
2017-12-27 14:25:47 -0800

    Ring buffer works as expected

--- lib/ring_buffer.rb
+++ lib/ring_buffer.rb
@@ -13,19 +13,23 @@ class RingBuffer
 
   # O(1)
   def [](index)
-    # raise "index out of bounds" if index >= length || index < 0
+    raise "index out of bounds" if index >= @length || index < 0
     # p self[6]
     converted_idx = (@start_idx + index) % capacity
-    p "#{@start_idx} #{index} #{capacity}, converted: #{converted_idx}, val: #{@store[converted_idx]}"
+    # p "#{@start_idx} #{index} #{capacity}, converted: #{converted_idx}, val: #{@store[converted_idx]}"
+    # raise "index out of bounds" if converted_idx < @start_idx || converted_idx > @start_idx + @length
     # p @store[6]
     @store[converted_idx]
   end
 
   # O(1)
   def []=(index, val)
+    
     # converted_idx = (@start_idx % @capacity)
     # p "index #{index} is equal to index #{converted_idx}"
     converted_idx = (@start_idx + index) % capacity
+    # raise "index out of bounds" if converted_idx < @start_idx || converted_idx > @start_idx + @length
+    # p self[converted_idx]
     # p "setting #{converted_idx} to #{val}"
     @store[converted_idx] = val
     # p "check: #{self[index]}"
@@ -33,12 +37,16 @@ class RingBuffer
 
   # O(1)
   def pop
-    # val = 
+    raise "index out of bounds" if @length < 1
+    val = self[@length - 1]
     @length -= 1
+    val 
   end
 
   # O(1) ammortized
   def push(val)
+    # raise "pop does not resize"
+    resize! if (@length + 1) > @capacity 
     @length += 1
     last_idx = @length - 1
     self[last_idx] = val
@@ -46,6 +54,8 @@ class RingBuffer
 
   # O(1)
   def shift
+    raise "index out of bounds" if @length < 1
+
     val = self[0]
     @start_idx += 1
     @length -= 1
@@ -55,13 +65,15 @@ class RingBuffer
 
   # O(1) ammortized
   def unshift(val)
+    # raise "unshift does not resize "
     # first_idx = @start_idx
+    resize! if (@length + 1) > @capacity 
     @start_idx -= 1
     @length += 1
     # p first_idx
-    p "unshifting #{val}"
+    # p "unshifting #{val}"
     self[0] = val
-    p "the first index is now: #{self[0]}"
+    # p "the first index is now: #{self[0]}"
   end
 
   protected
@@ -72,6 +84,16 @@ class RingBuffer
   end
 
   def resize!
+    new_capacity = @capacity * 2
+    new_store = StaticArray.new(new_capacity)
+    i = 0
+    while i < @length 
+      new_store[i] = self[i]
+      i += 1
+    end   
+    @capacity = new_capacity
+    @start_idx = 0
+    @store = new_store
   end
 end
 
